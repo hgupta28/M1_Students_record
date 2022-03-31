3f5396508b427e46ee9ea13faa49fff3cb765b31
hgupta28 <43040312+hgupta28@users.noreply.github.com>
2022-03-31 22:39:41 +0530

    Add files via upload

---
+++ 3_Implementation/inc/fun.h
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <string.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <windows.h>
+
+void SetColor(int ForgC)
+
+void ClearConsoleToColors(int ForgC, int BackC)
+
+void SetColorAndBackground(int ForgC, int BackC)
+
+void drawRectangle()
+
+void window()
+
+void get_password(char* pass)
+
+void use_pass_field()
+
+void print_heading(const char st[])
+
+void add_student()
+
+void search_student()
+
+void mod_student()
+
+void gen_marksheet()
+
+void delete_student()
+
+void main_window()
